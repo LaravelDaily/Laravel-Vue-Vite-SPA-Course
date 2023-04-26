b95698da8ebb0c53c3cd21b93b708804e6d2000b
Nerijus <krekas@gmail.com>
2023-04-26 15:06:54 +0300

    Install Laravel + Vue: Load First Vue.js Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import PostsIndex from './components/Posts/Index.vue'
+
+createApp({})
+    .component('PostsIndex', PostsIndex)
+    .mount('#app')
