861071205fcb6356277956fc052dc3e756f83b9d
Nerijus <krekas@gmail.com>
2023-04-29 22:29:35 +0300

    SweetAlert for Screen Alert Notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,9 @@ import { createApp } from 'vue'
 import App from './layouts/App.vue'
 
 import router from './routes/index'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 createApp(App)
     .use(router)
+    .use(VueSweetalert2)
     .mount('#app')
